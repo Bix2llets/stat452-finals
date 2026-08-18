b57298cbfcad53c3292452fccb035bd06f39d4ed
Nguyen Hoang Thuan Phat <thuanphat4b@gmail.com>
2026-08-18 22:23:51 +0700

    Add descriptive statisitics on salary

--- activity1/step2/descriptive_statisitics.R
+++ activity1/step2/descriptive_statisitics.R
@@ -24,28 +24,47 @@ jarque.bera.test(data$salary_in_usd)
 ggplot(data, aes(salary_in_usd, experience_level)) +
   geom_boxplot() +
   theme_classic()
+ggsave("salary_by_exp.pdf")
 
+# There is a difference between workers of different experience level. From the boxplot, we see that the salary in used generally increases
+
+table(data$employment_type)
 ggplot(data, aes(salary_in_usd, employment_type)) +
   geom_boxplot() +
   theme_classic()
-ggplot(data, aes(salary_in_usd, work_year)) +
-  geom_point() +
+
+# There seem to be a large variance in the contract worker type. However, note that there are little observations in non-fulltime jobs, thus it is less accurate representation of the distirbution there.
+
+# C
+
+ggplot(data, aes(salary_in_usd, group = work_year)) +
+  geom_boxplot() +
   theme_classic()
+
+# In the first two year, there are no difference, but the final year have visible difference in salary (incrases)
+
+
 ggplot(data, aes(salary_in_usd, employee_residence)) +
   geom_boxplot() +
   theme_classic()
+
+# The observation group is fragmented and clutered in US. There are many 1-observation, thus we cannot conclude any on this
+table(data$company_location)
 ggplot(data, aes(salary_in_usd, company_location)) +
   geom_boxplot() +
   theme_classic()
+# Similarly, there are fragmentation and clustering in company location, with heavilty unbalanced observation. Therefore we cannot conclude any on this
 ggplot(data, aes(salary_in_usd, company_size)) +
   geom_boxplot() +
   theme_classic()
-ggplot(data, aes(salary_in_usd, discipline)) +
-  geom_boxplot() +
-  theme_classic()
+
+# The small companies seem to offer loweest salary, while the medium companies offer highest salary, differs from the large companies' salary slightly.
 ggplot(data, aes(salary_in_usd, leadership)) +
   geom_boxplot() +
   theme_classic()
+# The boxplot shows those being at leader / management role has higher salary than those are not
 ggplot(data, aes(salary_in_usd, role)) +
   geom_boxplot() +
   theme_classic()
+
+# There seem to be no signficant differences between roles in data science.
