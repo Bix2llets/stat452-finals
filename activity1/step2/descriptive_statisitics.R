f004bea8d7be896a155d9c0b6577ae72079595f7
Nguyen Hoang Thuan Phat <thuanphat4b@gmail.com>
2026-08-20 14:37:56 +0700

    Add pictures

--- activity1/step2/descriptive_statisitics.R
+++ activity1/step2/descriptive_statisitics.R
@@ -11,6 +11,7 @@ str(data)
 summary(data$salary_in_usd)
 ggplot(data, aes(salary_in_usd)) +
   geom_histogram(bins = 20)
+ggsave("salary_hisotogram.pdf")
 
 
 ggplot(data, aes(salary_in_usd)) +
@@ -28,10 +29,10 @@ ggsave("salary_by_exp.pdf")
 
 # There is a difference between workers of different experience level. From the boxplot, we see that the salary in used generally increases
 
-table(data$employment_type)
 ggplot(data, aes(salary_in_usd, employment_type)) +
   geom_boxplot() +
   theme_classic()
+ggsave("salary_by_employment.pdf")
 
 # There seem to be a large variance in the contract worker type. However, note that there are little observations in non-fulltime jobs, thus it is less accurate representation of the distirbution there.
 
@@ -40,31 +41,38 @@ ggplot(data, aes(salary_in_usd, employment_type)) +
 ggplot(data, aes(salary_in_usd, group = work_year)) +
   geom_boxplot() +
   theme_classic()
+ggsave("salary_by_work_year.pdf")
 
 # In the first two year, there are no difference, but the final year have visible difference in salary (incrases)
 
 
+table(data$employee_residence)
 ggplot(data, aes(salary_in_usd, employee_residence)) +
   geom_boxplot() +
   theme_classic()
+ggsave("salary_by_employee_residence.pdf")
 
 # The observation group is fragmented and clutered in US. There are many 1-observation, thus we cannot conclude any on this
 table(data$company_location)
 ggplot(data, aes(salary_in_usd, company_location)) +
   geom_boxplot() +
   theme_classic()
+ggsave("salary_by_company_location.pdf")
 # Similarly, there are fragmentation and clustering in company location, with heavilty unbalanced observation. Therefore we cannot conclude any on this
 ggplot(data, aes(salary_in_usd, company_size)) +
   geom_boxplot() +
   theme_classic()
 
+ggsave("salary_by_company_size.pdf")
 # The small companies seem to offer loweest salary, while the medium companies offer highest salary, differs from the large companies' salary slightly.
 ggplot(data, aes(salary_in_usd, leadership)) +
   geom_boxplot() +
   theme_classic()
+ggsave("salary_by_leadership_role.pdf")
 # The boxplot shows those being at leader / management role has higher salary than those are not
 ggplot(data, aes(salary_in_usd, role)) +
   geom_boxplot() +
   theme_classic()
+ggsave("salary_by_role.pdf")
 
-# There seem to be no signficant differences between roles in data science.
+# There seem to be no signficant differences between mean salary of roles in data science.. However, those being analyst has its 3rd quartile being lower than the other. We can't conclude anything on the Unspecified roles, since its sample size is small and their role are not explicitly mentioned.
